0f10ddb5da851978c0a98dccc47a9b46fea0e520
Joao Vitor <jvmelop@gmail.com>
2025-08-10 22:51:34 -0300

    feat: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Kudwa Dashboard",
   description: "Check out Kudwa's dashboard and reports",
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
